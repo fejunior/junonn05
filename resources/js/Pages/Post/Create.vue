63e69a6b0f454033d93c596c129fd3ddbf8be0dd
Fernando Gomes de Sousa Junior <fejunior5@hotmail.com>
2023-01-01 22:31:10 -0300

    junonn05prj

---
+++ resources/js/Pages/Post/Create.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="">
+
+    <form @submit.prevent="submit">
+      <div class="flex flex-col">
+
+        <label for="title"> Título</label>
+        <input v-model="form.title" type="text" name="title" id="title">
+        <label for="subtitle"> Subtítulo</label>
+        <input v-model="form.subtitle" type="text" name="title" id="title">
+        <label for="content"> Título</label>
+        <textarea v-model="form.content" name="content" id="content" cols="30" rows="10"></textarea>
+        <button :disabled="form.processing" type="submit">Salvar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup >
+import { reactive } from 'vue'
+import { Inertia } from '@inertiajs/inertia'
+
+
+defineProps({
+  user: Object,
+});
+
+
+const form = reactive({
+  title: null,
+  subtitle: null,
+  content: null,
+})
+
+async function submit () {
+  await Inertia.post(route('post.store'), form, {
+    onSuccess: (page) => {
+      console.log("chegou a qui");
+      console.log(page);
+    }
+  });
+
+}
+
+
+
+
+</script>
